0c8a9b4c7be225fd66f54d851f18c971cc572c06
rulessbroker <rulessbroker>
2023-01-20 13:56:57 +0530

    modified: post request method and pass first and last name as query parameter

--- src/main/java/com/RestApiCalls/Contoller/HelloWebRestController.java
+++ src/main/java/com/RestApiCalls/Contoller/HelloWebRestController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.RestApiCalls.Data.Entity;
+
 @RestController
 @RequestMapping(value={"/home","/"},method = RequestMethod.GET)
 public class HelloWebRestController {
@@ -15,5 +17,10 @@ public class HelloWebRestController {
     @RequestMapping(value = {"/{name}/{email}"},method = RequestMethod.GET)
     public String getEmail(@PathVariable String email){
         return "<h1>Your "+email+" ID is required!</h1>";
+    }
+    
+    @PostMapping("/post")
+    public String post(@RequestBody Entity entity){
+        return "Welcome "+entity.getFirstName()+" "+entity.getLastName()+"!!!";
     }
 }
---
+++ src/main/java/com/RestApiCalls/Data/Entity.java
@@ -0,0 +1,18 @@
+package com.RestApiCalls.Data;
+
+public class Entity {
+	String firstName;
+    String lastName;
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setFirstName(String firstName){
+        this.firstName=firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public void setLastName(String lastName){
+        this.lastName=lastName;
+    }
+}
