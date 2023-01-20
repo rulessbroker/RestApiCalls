4e8b1a63fa3c298abe690a5a23cd67410c5eccbe
rulessbroker <rulessbroker>
2023-01-20 14:00:22 +0530

    Modified: Put request method

--- src/main/java/com/RestApiCalls/Contoller/HelloWebRestController.java
+++ src/main/java/com/RestApiCalls/Contoller/HelloWebRestController.java
@@ -23,4 +23,10 @@ public class HelloWebRestController {
     public String post(@RequestBody Entity entity){
         return "Welcome "+entity.getFirstName()+" "+entity.getLastName()+"!!!";
     }
+    
+    @PutMapping("/put/{firstName}")
+    public String put(@PathVariable String firstName,
+                      @RequestParam(value = "lastName") String lastName) {
+        return "Hello " + firstName + " " + lastName + "!";
+    }
 }
